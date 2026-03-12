f287e00491b76e741e56a898bf1411322fd42cc1
frosthowl <122034407+rohan-005@users.noreply.github.com>
2026-03-12 23:20:40 +0530

    Time: 1309 ms (5.06%), Space: 153.6 MB (5%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
+    // int totalVal(string s) {
+    //     int ans = 0;
+    //     for (int i = 0; i < s.length(); i++) {
+    //         ans += (int)s[i];
+    //     }
+
+    //     return ans;
+    // }
     bool checkInclusion(string s1, string s2) {
-
+        // int cs1 = 0, cs2 = 0;
         int n = s1.length();
-        int m = s2.length();
-
-        if (n > m) return false;
-
+        // cs1 = totalVal(s1);
+        if (n>s2.length()) return false;
         sort(s1.begin(), s1.end());
-
-        for (int i = 0; i <= m - n; i++) {
-
+        for (int i = 0; i <= s2.length()-n; i++) {
             string a = "";
-
             for (int j = i; j < i + n; j++) {
                 a += s2[j];
             }
-
+            // cs2 = totalVal(a);
             sort(a.begin(), a.end());
-
-            if (a == s1)
+            if (s1 == a)
                 return true;
+            // if(cs2==cs1){
+            //     return true;
+            // }
         }
-
+        // cout << cs1 << " " << cs2;
         return false;
     }
 };
